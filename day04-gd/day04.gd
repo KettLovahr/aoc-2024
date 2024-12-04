217716b119abbdb3cc8689d37dfbb234ac9db7e7
KettLovahr <catusfelony@gmail.com>
2024-12-04 11:16:48 -0300

    Trim Day 4 solution a little

--- day04-gd/day04.gd
+++ day04-gd/day04.gd
@@ -1,15 +1,14 @@
 extends MainLoop
 
 var xmas_offsets: Array[Array] = [
-	[ Vector2i(0, 0), Vector2i(1, 0), Vector2i(2, 0), Vector2i(3, 0) ],
-	[ Vector2i(0, 0), Vector2i(0, 1), Vector2i(0, 2), Vector2i(0, 3) ],
-	[ Vector2i(0, 0), Vector2i(1, 1), Vector2i(2, 2), Vector2i(3, 3) ],
-	[ Vector2i(0, 0), Vector2i(-1, 1), Vector2i(-2, 2), Vector2i(-3, 3) ],
+	[ [0, 0], [1, 0], [2, 0], [3, 0] ],
+	[ [0, 0], [0, 1], [0, 2], [0, 3] ],
+	[ [0, 0], [1, 1], [2, 2], [3, 3] ],
+	[ [0, 0], [-1, 1], [-2, 2], [-3, 3] ],
 ]
-
 var x_mas_offsets: Array[Array] = [
-	[ Vector2i(-1, -1), Vector2i(0, 0), Vector2i(1, 1), ],
-	[ Vector2i(-1, 1), Vector2i(0, 0), Vector2i(1, -1), ],
+	[ [-1, -1], [0, 0], [1, 1], ],
+	[ [-1, 1], [0, 0], [1, -1], ],
 ]
 
 func _initialize():
@@ -18,22 +17,16 @@ func _initialize():
 	var xmas_matches: int = 0
 	var x_mas_matches: int = 0
 
-	var height: int = len(content)
-	var width: int = len(content[0])
-
-	for i in height:
-		for j in width:
+	for i in len(content):
+		for j in len(content[i]):
 			xmas_matches += check_xmas(content, i, j)
 			x_mas_matches += check_x_mas(content, i, j)
 
 	print(xmas_matches)
 	print(x_mas_matches)
 
-func safe_index(grid: PackedStringArray, line: int, row: int) -> String:
-	var height: int = len(grid)
-	var width: int = len(grid[0])
-
-	if line >= height or line < 0 or row >= width or row < 0:
+func g_index(grid: PackedStringArray, line: int, row: int) -> String:
+	if line >= len(grid) or line < 0 or row >= len(grid[0]) or row < 0:
 		return ""
 	return grid[line][row]
 
@@ -42,20 +35,19 @@ func check_xmas(grid: PackedStringArray, line: int, row: int) -> int:
 	for pat in xmas_offsets:
 		var word: String = ""
 		for offset in pat:
-			word += safe_index(grid, line + offset.y, row + offset.x)
+			word += g_index(grid, line + offset[1], row + offset[0])
 		if word == "XMAS" or word == "SAMX":
 			matches += 1
 	return matches
 
 func check_x_mas(grid: PackedStringArray, line: int, row: int) -> int:
-	var matches: int = 0
 	for pat in x_mas_offsets:
 		var word: String = ""
 		for offset in pat:
-			word += safe_index(grid, line + offset.y, row + offset.x)
-		if word == "MAS" or word == "SAM":
-			matches += 1
-	return 1 if matches == 2 else 0
+			word += g_index(grid, line + offset[1], row + offset[0])
+		if word != "MAS" and word != "SAM":
+			return 0
+	return 1
 
 func _process(_delta: float) -> bool: 
 	return true
